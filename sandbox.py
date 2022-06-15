9e34ec6ac49af13c8f8f0fc4b3416b893164c9e5
David van Buiten <david.buiten@gmail.com>
2022-06-15 11:43:29 +0200

    test

--- sandbox.py
+++ sandbox.py
@@ -3,4 +3,6 @@ import os
 if __name__ == "__main__":
     print('--- start ---')
     print(os.getenv('TEST_VAR', 'Nope'))
+    print('---')
+    print(os.getenv('IS_GITHUB_WORKFLOW', 'Nope'))
     print('--- end ---')
