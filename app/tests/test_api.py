8f9396ac672bd5037d0e991f6db5a4840f7023c1
David van Buiten <david.vanbuiten@gmail.com>
2023-12-15 13:51:37 +0100

    refactor: PyTest fixtures and coverage

--- app/tests/test_api.py
+++ app/tests/test_api.py
@@ -1,18 +1,14 @@
 from starlette.testclient import TestClient
 
-from app.api import app
 from app.version import __version__
 
-client = TestClient(app)
-
-
-def test_root():
+def test_root(client: TestClient):
     response = client.get('/')
     assert response.status_code == 200
     assert response.json() == {'version': __version__}
 
 
-def test_health():
+def test_health(client: TestClient):
     response = client.get('/health')
     assert response.status_code == 200
     assert response.json() == {'msg': "I'm healthy!"}
